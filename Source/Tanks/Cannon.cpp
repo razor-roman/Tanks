9258bb9bc19bfce4db539d9035cc14a28364a8a5
razor-roman <roman.it.kirov@yandex.ru>
2022-05-22 17:09:04 +0300

    1

---
+++ Source/Tanks/Cannon.cpp
@@ -0,0 +1,47 @@
+#include "Cannon.h"
+#include "Components/ArrowComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "TimerManager.h"
+#include "Engine/Engine.h"
+ACannon::ACannon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	USceneComponent * sceeneCpm =
+	CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = sceeneCpm;
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cannon mesh"));
+	Mesh->SetupAttachment(RootComponent);
+	ProjectileSpawnPoint = CreateDefaultSubobject<UArrowComponent>(TEXT("Spawn point"));
+	ProjectileSpawnPoint->SetupAttachment(Mesh);
+	}
+void ACannon::Fire()
+{
+	if(!ReadyToFire)
+	{
+		return;
+	}
+	ReadyToFire = false;
+	if(Type == ECannonType::FireProjectile)
+	{
+		GEngine->AddOnScreenDebugMessage(10, 1,FColor::Green, "Fire -	projectile");
+		}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(10, 1,FColor::Green, "Fire - trace");
+	}
+	GetWorld()->GetTimerManager().SetTimer(ReloadTimerHandle, this,
+    &ACannon::Reload, 1 / FireRate, false);
+    }
+    bool ACannon::IsReadyToFire()
+    {
+    return ReadyToFire;
+    }
+    void ACannon::Reload()
+    {
+    ReadyToFire = true;
+    }
+    void ACannon::BeginPlay()
+    {
+    Super::BeginPlay();
+    Reload();
+    }
